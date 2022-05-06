e9e18fc799bc782be6289246533cbc758776d642
saheed007 <saheedoladejo007@gmail.com>
2022-05-06 22:04:05 +0100

    Update WordGuesser.py

--- WordGuesser.py
+++ WordGuesser.py
@@ -3,47 +3,47 @@ import random
 print("Welcome to Word Guesser. \nYou have just three tries.\nEnter your guesses accordingly")
 
 trials=1
-word_set = (("m", "a", "n"), ("m", "e", "n"), ("p", "a", "n"), ("l", "a", "d"), ("b", "i", "n"), ("s", "i", "t"), ("c", "a", "p"), ("g", "o", "t"), ("t", "o", "y"))
+word_set = ["pan", "man", "sit", "cut", "son", "den", "try", "day"]
+word = random.choice(list(word_set))
+print(word)
 
-word = random.choice(tuple(word_set))
-f_g1=()
-while trials <= 5 and word[:] != f_g1[:]:
-    f_g=input("Input your guess: ")
-    f_g1= (f_g[0], f_g[1], f_g[2])
+guess=""
+while trials <= 5 and word != guess:
+    guess=input("Input your guess: ")
 
     if trials == 1:
-        print("First guess: "+ f_g )
+        print("First guess: "+ guess )
     if trials == 2:
-        print("Second guess: "+ f_g )
+        print("Second guess: "+ guess )
     if trials == 3:
-        print("Third guess: "+ f_g )
+        print("Third guess: "+ guess )
     if trials == 4:
-        print("Fourth guess: "+ f_g )
+        print("Fourth guess: "+ guess ) 
     if trials == 5:
-        print("Last guess: "+ f_g )
+        print("Last guess: "+ guess )
 
-    if f_g1[0] == word[0] and f_g1[1] != word[1] and f_g1[2] != word[2]:
+    if guess[0] == word[0] and guess[1] != word[1] and guess[2] != word[2]:
         print("You got only the first letter right: " + word[0]+ "--")
 
-    if f_g1[0] == word[0] and f_g1[1] == word[1] and f_g1[2] != word[2]:
+    if guess[0] == word[0] and guess[1] == word[1] and guess[2] != word[2]:
         print("You got only the first two letters right: " + word[0]+word[1]+ "-")
 
-    if f_g1[0] == word[0] and f_g1[1] != word[1] and f_g1[2] == word[2]:
+    if guess[0] == word[0] and guess[1] != word[1] and guess[2] == word[2]:
         print("You got only two letters right: " + word[0]+ "-"+word[2])
 
-    if f_g1[0] != word[0] and f_g1[1] == word[1] and f_g1[2] != word[2]:
+    if guess[0] != word[0] and guess[1] == word[1] and guess[2] != word[2]:
         print("You got only the middle letter right: -" + word[1]+ "-")
             
-    if f_g1[0] != word[0] and f_g1[1] == word[1] and f_g1[2] == word[2]:
+    if guess[0] != word[0] and guess[1] == word[1] and guess[2] == word[2]:
         print("You got only the last two letters right: -" + word[1] + word[2])
 
-    if f_g1[0] != word[0] and f_g1[1] != word[1] and f_g1[2] == word[2]:
+    if guess[0] != word[0] and guess[1] != word[1] and guess[2] == word[2]:
         print("You got only the last letter right: --" + word[2])
 
-    if f_g1[0] != word[0] and f_g1[1] != word[1] and f_g1[2] != word[2]:
+    if guess[0] != word[0] and guess[1] != word[1] and guess[2] != word[2]:
         print("You did not get any letter right: ---")        
 
-    if word[:] == f_g1[:]:
+    if word[:] == guess[:]:
         print("Correct!!!. You got all the letters right: "+word[0]+word[1]+word[2])
         break
     trials += 1
